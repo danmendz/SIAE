b5648d65ba4813164936601795aff5c36c790d36
danmendz <daniel.mdz.33palacios@gmail.com>
2025-08-20 21:04:48 -0600

    fix(calificacionResource): modificada la respuesta JSON del resource para calificaciones

--- app/Http/Resources/CalificacionResource.php
+++ app/Http/Resources/CalificacionResource.php
@@ -19,10 +19,14 @@ class CalificacionResource extends JsonResource
             'materia' => $this->materia,
             'profesor' => $this->profesor,
             'p1' => $this->p1,
+            'aro1_1' => $this->aro1_1,
             'p2' => $this->p2,
+            'aro2_1' => $this->aro1_1,
             'p3' => $this->p3,
+            'aro3_1' => $this->aro1_1,
             'ti' => $this->ti,
             'cf' => $this->cf,
+            'are1' => $this->are1,
             'calificacion_cuatrimestral' => $this->calificacion_cuatrimestral,
         ];
     }
